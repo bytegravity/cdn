b5502c4562ecb37c284d475cc584a24225a01d48
Ray <tvvocold@gmail.com>
2021-03-25 11:33:48 +0800

    Add files via upload

---
+++ wwads.js
@@ -0,0 +1,7 @@
+(function(){
+var _sr = document.createElement('script');
+_sr.type = 'text/javascript';
+_sr.async = true;
+_sr.src = 'https://wwads.cn/code/install';
+(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(_sr);
+})();
