e390cd3358b757f4a8525ab5a804e8ebce78e870
Ray <tvvocold@gmail.com>
2021-03-27 18:51:01 +0800

    Update wwads.js

--- wwads.js
+++ wwads.js
@@ -1,7 +1,24 @@
+//万维广告 js
+
 (function(){
 var _sr = document.createElement('script');
 _sr.type = 'text/javascript';
 _sr.async = true;
 _sr.src = 'https://wwads.cn/code/install';
 (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(_sr);
+})();
+
+// 百度 push 提交 url
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
 })();
