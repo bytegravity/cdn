3310a9d7516a764d9b4c502f62598ab55c64c68b
Ray <tvvocold@gmail.com>
2021-04-28 18:28:20 +0800

    Update wwads.js

--- wwads.js
+++ wwads.js
@@ -1,12 +1,6 @@
 // 万维广告 js
 
-(function(){
-var _sr = document.createElement('script');
-_sr.type = 'text/javascript';
-_sr.async = true;
-_sr.src = 'https://wwads.cn/code/install';
-(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(_sr);
-})();
+function docReady(t){"complete"===document.readyState||"interactive"===document.readyState?setTimeout(t,1):document.addEventListener("DOMContentLoaded",t)}function _AdBlockInit(){$ids=[],$blocks=document.getElementsByClassName("wwads-cn");var t=setInterval(function(){if($blocks){clearInterval(t);for(var e=0;e<$blocks.length;e++)$block=$blocks[e],$id=$block.getAttribute("data-id"),$block.setAttribute("id","wwads-cn-"+$id),$ids.push($id);_SendAdRequest($ids)}},300)}function _SendAdRequest(t){$response=[],request=new XMLHttpRequest,request.open("GET","https://wwads.cn/code/banners?blocks="+t,!0),request.onload=function(){if(request.status>=200&&request.status<400){data=JSON.parse(request.responseText);for(var t=0;t<data.blocks.length;t++)document.getElementById("wwads-cn-"+data.blocks[t].id).innerHTML=data.blocks[t].html}},request.onerror=function(){document.getElementById("wwads-cn-"+data.blocks[k].id).innerHTML="Connection Failed!"},request.send()}docReady(function(){_AdBlockInit()}),function(t){var e=t.pushState;t.pushState=function(n){return"function"==typeof t.onpushstate&&t.onpushstate({state:n}),e.apply(t,arguments)}}(window.history),window.onpopstate=history.onpushstate=function(t){setTimeout(function(){_AdBlockInit()},1e3)};
 
 // 百度 push 提交 url
 
