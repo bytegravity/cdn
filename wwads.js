e83dfec593d8faa7c6ebdab94c933eea2519f960
Ray <tvvocold@gmail.com>
2021-07-09 22:36:10 +0800

    Update wwads.js

--- wwads.js
+++ wwads.js
@@ -2,21 +2,28 @@
 
 function docReady(t){"complete"===document.readyState||"interactive"===document.readyState?setTimeout(t,1):document.addEventListener("DOMContentLoaded",t)}function _AdBlockInit(){$ids=[];var t=setInterval(function(){if($blocks=document.getElementsByClassName("wwads-cn"),$blocks.length>0){clearInterval(t);for(var e=0;e<$blocks.length;e++)$block=$blocks[e],$id=$block.getAttribute("data-id"),$block.setAttribute("id","wwads-cn-"+$id),$ids.push($id);_SendAdRequest($ids)}},300)}function _SendAdRequest(t){$response=[],request=new XMLHttpRequest,request.open("GET","https://wwads.cn/code/banners?blocks="+t,!0),request.onload=function(){if(request.status>=200&&request.status<400){data=JSON.parse(request.responseText);for(var t=0;t<data.blocks.length;t++)document.getElementById("wwads-cn-"+data.blocks[t].id).innerHTML=data.blocks[t].html}},request.onerror=function(){document.getElementById("wwads-cn-"+data.blocks[k].id).innerHTML="Connection Failed!"},request.send()}docReady(function(){_AdBlockInit()}),function(t){var e=t.pushState;t.pushState=function(n){return"function"==typeof t.onpushstate&&t.onpushstate({state:n}),e.apply(t,arguments)}}(window.history),window.onpopstate=history.onpushstate=function(t){setTimeout(function(){_AdBlockInit()},1e3)};
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-      function loadGoogleAds() {
-        googleads = document.getElementsByClassName("wwads-cn")[0].getElementsByTagName("script");
-        eval(googleads[0].innerHTML);
-      }
-      //trying to find the google ins with the class name "adsbygoogle" every 1ms, if find then fire the loadGoogleAds function
-      var timer = setInterval(function () {
-        var adsIns = document.getElementsByClassName("adsbygoogle");
-        if (adsIns.length > 0) {
-          clearInterval(timer);
-          loadGoogleAds(adsIns);
-        }
-      }, 1);
-</script>
+//load the googleads js
+(function(){
+var _sr = document.createElement('script');
+_sr.type = 'text/javascript';
+_sr.async = true;
+_sr.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(_sr);
+})();
+function loadGoogleAds() {
+    googleads = document.getElementsByClassName("wwads-cn")[0].getElementsByTagName("script");
+    eval(googleads[0].innerHTML);
+}
+//trying to find the google ins with the class name "adsbygoogle" every 1ms, if find then fire the loadGoogleAds function
+var timer = setInterval(function() {
+    var adsIns = document.getElementsByClassName("adsbygoogle");
+    if (adsIns.length > 0) {
+        clearInterval(timer);
+        loadGoogleAds(adsIns);
+    }
+},
+1);
+
 
 
 // 百度 push 提交 url
